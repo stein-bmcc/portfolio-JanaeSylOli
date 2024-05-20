f4aef4927c041cd47ac5b210cb3a80c0d414f4cc
JanaeSylOli <147261259+JanaeSylOli@users.noreply.github.com>
2024-05-20 23:56:16 +0000

    updated js query

--- .eleventy.js
+++ .eleventy.js
@@ -4,9 +4,9 @@ const eleventyNavigationPlugin = require('@11ty/eleventy-navigation')
 
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addPassthroughCopy('./_src/images');
-  eleventyConfig.addPassthroughCopy('./_src/css');
-  eleventyConfig.addPassthroughCopy('./_src/js');
+  eleventyConfig.addPassthroughCopy('./src/images');
+  eleventyConfig.addPassthroughCopy('./src/css');
+  eleventyConfig.addPassthroughCopy('./src/js');
 
   eleventyConfig.addLayoutAlias('base', 'pageTemplates/base.njk');
   eleventyConfig.addLayoutAlias('page', 'pageTemplates/page.njk');
